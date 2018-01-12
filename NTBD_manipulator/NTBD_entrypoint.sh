01cf7ae372b82c9aa23634fef0a5fe9d685a4bc4
Fiorella Sibona <fiorella.sibona@gmail.com>
2018-01-12 14:14:37 +0100

    january commit

--- NTBD_manipulator/NTBD_entrypoint.sh
+++ NTBD_manipulator/NTBD_entrypoint.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -e
 echo "export TERM=xterm" >> ~/.bashrc
+# setup ros environment in each command line window
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+echo "source /catkin_ws/devel/setup.bash" >> ~/.bashrc
 # setup ros environment
- source /opt/ros/kinetic/setup.bash
- source /catkin_ws/devel/setup.bash
-
+source /opt/ros/kinetic/setup.bash
+source /catkin_ws/devel/setup.bash
 # start nginx
 service nginx start
-
 # Launch my ROS nodes and ros3djs URDF visualization
 roslaunch ntbd_core NTBD_launch.launch
 exec "$@"
