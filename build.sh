c74870709e02c4f13b5f89e8508f0bb7d555abc1
Fiorella Sibona <fiorella.sibona@gmail.com>
2018-01-17 10:28:00 +0100

    deleted build.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-docker build -f NTBD_base/Dockerfile.rpi3 -t hbrobotics/ntbd_base:rpi3 ./NTBD_base/
-docker build -f NTBD_manipulator/Dockerfile.rpi3 -t hbrobotics/ntbd_manipulator:rpi3 ./NTBD_manipulator/
-docker push hbrobotics/ntbd_base:rpi3
-docker push hbrobotics/ntbd_manipulator:rpi3
